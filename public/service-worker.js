9e1f032579849770b75253dd17fef83fc6f361c6
debsdebsenson <flitzifling@posteo.de>
2024-10-02 14:16:09 +0200

    added handling for push notifications to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,3 +20,13 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
+
+// handle push notifications
+self.addEventListener('push', (event) => {
+    const data = event.data.json();
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icons/icon-192x192.png',
+    });
+  });
+  
